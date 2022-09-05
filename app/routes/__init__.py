88afae544e8206f33a95daa86d834ad47fffcadf
Mehrnaz Payami <mn.payami@gmail.com>
2022-09-05 12:28:46 +0000

    import route for json result

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
 from .classifications import *
 from .classifications_id import *
 from .classifications_using_upload import *
-# from .classifications_id_result import *
+from .classifications_id_result import *
 
 from .info import *
 from .home import *
