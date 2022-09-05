82dbe772e0f99ba58a9027cf0ebade744c9b6bde
Mehrnaz Payami <mn.payami@gmail.com>
2022-09-05 13:00:00 +0000

    added import for hiostogram route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,7 @@ from .classifications import *
 from .classifications_id import *
 from .classifications_using_upload import *
 from .classifications_id_result import *
+from .histogram import *
 
 from .info import *
 from .home import *
