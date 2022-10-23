4fa83d3b6babde0f8da84ca739642a63d3d96071
MarcoMulas99 <mrc.mls99@gmail.com>
2022-10-23 16:09:39 +0200

    Created new form for the image histogram functionality

---
+++ app/forms/image_histogram_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+from app.utils.list_images import list_images
+from config import Configuration
+
+
+
+class ClassificationForm(FlaskForm):
+    image = SelectField('image', choices=list_images(), validators=[DataRequired()])
+    submit = SubmitField('Submit')
